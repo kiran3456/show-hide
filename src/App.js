fadcdf1e2699c0030c1414faae7994cd576f5325
Kiran <kirandeepkaurfdk5@gmail.com>
2023-05-26 17:19:58 -0800

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [show, setshow] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      Hello world
+      <button onClick={() => setshow(!show)}>{show ? "Show" : "Hide"}</button>
+      {show ? (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Social media is the fastest-growing communication medium. Social media
+          comprises the latest communication and social networking sites through
+          the virtual medium of the internet. With the development of technology
+          and different social media platforms, the world looks smaller.
+          Nowadays, it isn’t easy to survive without social media. We need
+          social media to connect with people. To get a better idea of social
+          media and write a paragraph on the same, refer to the below-provided
+          sample paragraphs on social media.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ) : null}
     </div>
   );
 }
